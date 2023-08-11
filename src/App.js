c151b75faa481606634359760001af9583921a01
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2023-08-11 01:23:02 -0500

    feat(App, Error404): add fallback page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DarkThemeProvider from './provider/DarkThemeProvider';
 import { Layout } from './pages/Layout';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { Error404 } from './pages/Error404';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
+          <Route path='*' element={<Error404 />} />
         </Route>
       </Routes>
     </DarkThemeProvider>
